8c5ddc1526d2a00bb3d05c7d398788ed16436df7
eskelProgramming <eskelley0673@students.cptc.edu>
2024-05-25 13:52:14 -0700

    Create Game class

---
+++ typescript/game.ts
@@ -0,0 +1,13 @@
+/**
+ * Represents a single game.
+ */
+class Game {
+    /**
+     * The players in the game.
+     */
+    players: Player[];
+    /**
+     * The player whose turn it is.
+     */
+    currentPlayer: Player;
+}
