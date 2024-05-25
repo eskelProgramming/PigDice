2ad473fc966f7759241d79523a1c59ffe7df97db
eskelProgramming <eskelley0673@students.cptc.edu>
2024-05-25 13:40:34 -0700

    Create Player class

---
+++ typescript/player.ts
@@ -0,0 +1,17 @@
+/**
+ * Represents a single player.
+ */
+class Player {
+    /**
+     * The player's name.
+     */
+    name: string;
+    /**
+     * The player's total score.
+     */
+    score: number;
+    /**
+     * The player's current turn total.
+     */
+    turnTotal: number;
+}
