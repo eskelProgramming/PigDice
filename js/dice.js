55f6fa36ccde29bb6a7d65c9bbdcaeaffe3c9643
eskelProgramming <eskelley0673@students.cptc.edu>
2024-05-27 12:54:29 -0700

    Added newGame functionality

--- js/dice.js
+++ js/dice.js
@@ -1,3 +1,4 @@
+let currGame = new Game();
 function generateRandomValue(minValue, maxValue) {
     var random = Math.random();
     return random;
@@ -14,14 +15,23 @@ window.onload = function () {
     document.getElementById("hold").onclick = holdDie;
 };
 function createNewGame() {
-    let currGame = new Game();
+    currGame = new Game();
     currGame.players[0] = new Player();
     currGame.players[1] = new Player();
-    document.getElementById("turn").classList.add("open");
-    document.getElementById("total").value = "0";
-    document.getElementById("player1").setAttribute("disabled", "disabled");
-    document.getElementById("player2").setAttribute("disabled", "disabled");
-    changePlayers();
+    let player1Txt = document.getElementById("player1");
+    let player2Txt = document.getElementById("player2");
+    if (player1Txt.value.trim() == "" || player2Txt.value.trim() == "") {
+        alert("Please enter a name into both text boxes.");
+    }
+    else {
+        currGame.players[0].name = player1Txt.value;
+        currGame.players[1].name = player2Txt.value;
+        document.getElementById("turn").classList.add("open");
+        document.getElementById("total").value = "0";
+        (player1Txt).setAttribute("disabled", "disabled");
+        (player2Txt).setAttribute("disabled", "disabled");
+        changePlayers();
+    }
 }
 function rollDie() {
     let currTotal = parseInt(document.getElementById("total").value);
--- typescript/dice.ts
+++ typescript/dice.ts
@@ -1,13 +1,16 @@
-function generateRandomValue(minValue:number, maxValue:number):number{
+// Declare in the global scope
+let currGame: Game = new Game();
+
+function generateRandomValue(minValue: number, maxValue: number): number {
     var random = Math.random();
-    
+
     //TODO: use random to generate a number between min and max
 
     return random;
 }
 
 
-function changePlayers():void{
+function changePlayers(): void {
     let currentPlayerName = (<HTMLElement>document.getElementById("current")).innerText;
     let player1Name = (<HTMLInputElement>document.getElementById("player1")).value;
     let player2Name = (<HTMLInputElement>document.getElementById("player2")).value;
@@ -16,7 +19,7 @@ function changePlayers():void{
     //set currentPlayerName to the next player
 }
 
-window.onload = function(){
+window.onload = function () {
     let newGameBtn = document.getElementById("new_game") as HTMLButtonElement;
     newGameBtn.onclick = createNewGame;
 
@@ -25,30 +28,49 @@ window.onload = function(){
     (<HTMLButtonElement>document.getElementById("hold")).onclick = holdDie;
 }
 
-function createNewGame(){
-    //set player 1 and player 2 scores to 0
+function createNewGame() {
+    //reset the game object
+    currGame = new Game();
+    
+    // Create players in the game object
+    currGame.players[0] = new Player();
+    currGame.players[1] = new Player();
 
     //verify each player has a name
     //if both players don't have a name display error
+    let player1Txt = document.getElementById("player1") as HTMLInputElement;
+    let player2Txt = document.getElementById("player2") as HTMLInputElement;
 
-    //if both players do have a name start the game!
-    (<HTMLElement>document.getElementById("turn")).classList.add("open");
-    (<HTMLInputElement>document.getElementById("total")).value = "0";
-    //lock in player names and then change players
-    (<HTMLInputElement>document.getElementById("player1")).setAttribute("disabled", "disabled");
-    (<HTMLInputElement>document.getElementById("player2")).setAttribute("disabled", "disabled");
-    changePlayers();
+    if (player1Txt.value.trim() == "" || player2Txt.value.trim() == "") {
+        alert("Please enter a name into both text boxes.");
+    }
+
+    else {    
+        // set player names
+        currGame.players[0].name = player1Txt.value;
+        currGame.players[1].name = player2Txt.value;
+        
+
+        //if both players do have a name start the game!
+        (<HTMLElement>document.getElementById("turn")).classList.add("open");
+        (<HTMLInputElement>document.getElementById("total")).value = "0";
+        //lock in player names and then change players
+        (player1Txt).setAttribute("disabled", "disabled");
+        (player2Txt).setAttribute("disabled", "disabled");
+        changePlayers();
+    }
+
 }
 
-function rollDie():void{
+function rollDie(): void {
     let currTotal = parseInt((<HTMLInputElement>document.getElementById("total")).value);
-    
+
     //roll the die and get a random value 1 - 6 (use generateRandomValue function)
 
     //if the roll is 1
     //  change players
     //  set current total to 0
-    
+
     //if the roll is greater than 1
     //  add roll value to current total
 
@@ -56,7 +78,7 @@ function rollDie():void{
     //display current total on form
 }
 
-function holdDie():void{
+function holdDie(): void {
     //get the current turn total
     //determine who the current player is
     //add the current turn total to the player's total score
