27030b53ceec1e8178562aea7e91b8cb969512f2
eskelProgramming <eskelley0673@students.cptc.edu>
2024-05-27 12:18:14 -0700

    Add js files for dice, game, and player

---
+++ js/dice.js
@@ -0,0 +1,31 @@
+function generateRandomValue(minValue, maxValue) {
+    var random = Math.random();
+    return random;
+}
+function changePlayers() {
+    let currentPlayerName = document.getElementById("current").innerText;
+    let player1Name = document.getElementById("player1").value;
+    let player2Name = document.getElementById("player2").value;
+}
+window.onload = function () {
+    let newGameBtn = document.getElementById("new_game");
+    newGameBtn.onclick = createNewGame;
+    document.getElementById("roll").onclick = rollDie;
+    document.getElementById("hold").onclick = holdDie;
+};
+function createNewGame() {
+    let currGame = new Game();
+    currGame.players[0] = new Player();
+    currGame.players[1] = new Player();
+    document.getElementById("turn").classList.add("open");
+    document.getElementById("total").value = "0";
+    document.getElementById("player1").setAttribute("disabled", "disabled");
+    document.getElementById("player2").setAttribute("disabled", "disabled");
+    changePlayers();
+}
+function rollDie() {
+    let currTotal = parseInt(document.getElementById("total").value);
+}
+function holdDie() {
+    changePlayers();
+}
